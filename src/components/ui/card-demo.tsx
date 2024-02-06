dcf5f76f70f3ca47e57d56726ea033a1da59c9e4
Eryk Wójcik <101266606+pananananas@users.noreply.github.com>
2024-02-06 18:01:48 +0100

    added image

--- src/components/ui/card-demo.tsx
+++ src/components/ui/card-demo.tsx
@@ -10,16 +10,17 @@ export function ThreeDCardDemo() {
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-neutral-600 dark:text-white family-kapakana"
         >
-          Urodzinowy wałczer
+            <span className="font-family-kapakana">Wszystkiego najlepszego!!</span>
+
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          Otrzymujesz zaproszenie na obiadek urodzinowy. Możesz go wykorzystać w wybranym przez siebie lokalu.
         </CardItem>
         <CardItem
           translateZ="100"
@@ -28,7 +29,7 @@ export function ThreeDCardDemo() {
           className="w-full mt-4"
         >
           <Image
-            src="/images/3d-card/3d-card-thumbnail.jpg"
+            src="/resources/juwe.JPG"
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
